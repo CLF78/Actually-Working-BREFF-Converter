a0aeed7a53da393b9744b74609b2ed089e317597
CLF78 <29682335+CLF78@users.noreply.github.com>
2024-12-19 11:22:47 +0100

    Start implementing encoding support
    Currently only encodes the meta.json file

--- common/args.py
+++ common/args.py
@@ -7,9 +7,10 @@ import argparse
 from pathlib import Path
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Converts a BREFF file to a set of JSON files')
-    parser.add_argument('inputs', nargs='+', type=Path, help='The files to convert', action='append')
-    parser.add_argument('-o', '--outputs', nargs='*', type=Path, help='The output directory for each file')
+    parser = argparse.ArgumentParser(description='Converts a BREFF file to a set of JSON files and back')
+    parser.add_argument('operation', choices=['decode', 'encode'], help='The operation to execute')
+    parser.add_argument('inputs', nargs='+', type=Path, help='The files/directories to convert', action='append')
+    parser.add_argument('-o', '--outputs', nargs='*', type=Path, help='The output directory/file for each input')
     parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output')
     return parser.parse_args()
 
